8001c45d81ceb41799be8b2e545ab4537d9725dc
jhirschv <johnhirschv@gmail.com>
2023-11-29 17:20:53 -0500

    day 3 save

---
+++ script.js
@@ -0,0 +1,14 @@
+function onScroll() {
+    const elements = document.querySelectorAll('.tech-logos, .about-section');
+
+    for (const element of elements) {
+        const rect = element.getBoundingClientRect();
+        const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+
+        if (isVisible) {
+            element.classList.add('visible');
+        }
+    }
+}
+
+window.addEventListener('scroll', onScroll);
